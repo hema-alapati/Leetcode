3f8e08c573ca72851c3e6ce3f83aea233d8001f8
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-09-21 15:17:58 +0530

    Time: 30 ms (18.61%), Space: 17.1 MB (11.68%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,11 +1,11 @@
 class Solution {
 private:
     // Write code for dfs/bfs, mark the visited nodes
-    void dfs(int node, vector<vector<int>>& adjLs, vector<bool>& vis) {
-        vis[node] = true;
+    void dfs(int node, vector<int> adjLs[], vector<int>& vis) {
+        vis[node] = 1; 
         for (auto it : adjLs[node]) {
             if (!vis[it]) {
-                dfs(it, adjLs, vis);
+                dfs(it, adjLs, vis); 
             }
         }
     }
@@ -15,29 +15,30 @@ public:
         // Get the number of nodes
         int V = isConnected.size();
 
-        // Create an adjacency list
-        vector<vector<int>> adjLs(V);
-        // Change adjacency matrix to adjacency list
+        vector<int> adjLs[V]; 
+        
+        // Convert adjacency matrix to list 
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
+                // Self nodes are not considered
                 if (isConnected[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
                 }
             }
         }
+        
+        vector<int> vis(V, 0); // Initialize vis as a vector
 
-        // Create visited array
-        vector<bool> vis(V, false);
-        int cnt = 0;
-        // Traverse the list and call dfs if the node is not visited
+        int cnt = 0; 
         for (int i = 0; i < V; i++) {
+            // If the node is not visited
             if (!vis[i]) {
-                dfs(i, adjLs, vis);
-                // Increment count every time dfs is called
+                // Counter to count the number of provinces 
                 cnt++;
+                dfs(i, adjLs, vis); 
             }
         }
-        return cnt;
+        return cnt; 
     }
 };
