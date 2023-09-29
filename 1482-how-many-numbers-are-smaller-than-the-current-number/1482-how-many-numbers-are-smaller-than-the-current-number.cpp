32085087db1bd8ebaf97af902720f64af8567f1a
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-09-30 01:57:26 +0530

    Time: 10 ms (74.92%), Space: 11.1 MB (5.42%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        map<int, int> m;
+        vector<int> vec = nums;
+        int n = nums.size();
+
+        sort(vec.begin(), vec.end());
+        // Put the values in hash-map according to their values corresponding to their places in nums 
+        for(int i=n-1;i>=0;i--) {
+            m[vec[i]] = i;
+        }
+        // Saving the result again in nums for returning
+            for(int i=0; i<n; i++) {
+            nums[i] = m[nums[i]];
+        }
+        return nums;
+    }
+};
