4d21532554b02ff4f86040e9096d8bf42634e37b
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-11 12:51:50 +0530

    Time: 239 ms (72.41%), Space: 82.3 MB (80.4%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        vector<int> start, end;
-        for (auto& t : flowers)
-            start.push_back(t[0]), end.push_back(t[1]);
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int>start,end;
+        for(vector<int> &flower:flowers){
+            start.push_back(flower[0]);
+            end.push_back(flower[1]);
+        }
         sort(start.begin(), start.end());
         sort(end.begin(), end.end());
-        vector<int> res;
-        for (int t : persons) {
-            int started = upper_bound(start.begin(), start.end(), t) - start.begin();
-            int ended = lower_bound(end.begin(), end.end(), t) - end.begin();
-            res.push_back(started - ended);
+        vector<int>res;
+        for(auto t:people){
+            //subtracting with starting pointer to get relative index value to first
+            int started=upper_bound(start.begin(), start.end(), t)-start.begin();
+            //upper_bound returns an iterator that points to index of element greater than given value
+            int ended=lower_bound(end.begin(),end.end(), t)-end.begin();
+            //lower_bound returns an iterator that points to index of element equal to given value
+            res.push_back(started-ended);
         }
         return res;
+        // Note: Sorting is a prerequisite for these functions (upper_bound and lower_bound) to work correctly.
     }
 };
