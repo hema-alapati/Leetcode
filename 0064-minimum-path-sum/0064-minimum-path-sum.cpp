cfb9ce8cf68db993955e309046ae05b539b71a85
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-12 12:40:00 +0530

    Time: 3 ms (98.01%), Space: 10.3 MB (30.9%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,33 +3,29 @@ public:
 int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0)); // Initialize a DP table to store minimum path sums
+        vector<int>prev(m,0);
 
         for (int i = 0; i < n; i++) {
+            vector<int>temp(m,0);
             for (int j = 0; j < m; j++) {
                 if (i == 0 && j == 0)
-                    dp[i][j] = grid[i][j]; // If we are at the top-left corner, the minimum path sum is the value at (0, 0)
+                    temp[j] = grid[i][j];
                 else {
-                    // Calculate the minimum path sum considering moving up and moving left
                     int up = grid[i][j];
                     if (i > 0)
-                        up += dp[i - 1][j]; // Include the minimum path sum from above
+                        up += prev[j];
                     else
-                        up += 1e9; // A large value if moving up is not possible (out of bounds)
-
+                        up += 1e9;
                     int left = grid[i][j];
                     if (j > 0)
-                        left += dp[i][j - 1]; // Include the minimum path sum from the left
+                        left += temp[j - 1]; 
                     else
-                        left += 1e9; // A large value if moving left is not possible (out of bounds)
-
-                    // Store the minimum path sum in dp[i][j]
-                    dp[i][j] = min(up, left);
+                        left += 1e9; 
+                    temp[j] = min(up, left);
                 }
             }
+            prev=temp;
         }
-
-        // The final result is stored in dp[n-1][m-1], which represents the destination
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 };
