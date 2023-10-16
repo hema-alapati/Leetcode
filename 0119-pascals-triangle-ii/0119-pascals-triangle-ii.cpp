14e06804ceb092d458de4d024e54a1c27f54daaa
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-16 17:47:07 +0530

    Time: 2 ms (41.13%), Space: 6.7 MB (51.35%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -6,6 +6,7 @@ public:
         for (int i = 1; i <= rowIndex; i++) {
             long long res=result[i-1];
             res*=(rowIndex-i+1);
+            //+1 because 4th row has an index 3 here, and parameter passed is 3
             res/=i;
             result.push_back(res);
         }
