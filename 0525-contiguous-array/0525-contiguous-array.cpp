cf90d86fdb4b0de9f5164b9e0a40308991c94269
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-16 22:45:50 +0530

    Time: 95 ms (51.01%), Space: 84.1 MB (39.65%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<int,int>sums;
         int sum=0;
         int maxLen=0;
-        sums[0]=-1;
-
+        // sums[0]=-1;
         for(int i=0;i<nums.size();i++){
             nums[i]==0?nums[i]=-1:nums[i]=1;
         }
@@ -13,12 +12,14 @@ public:
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
 
-            // if(sum==0){
-            //     if(maxLen<i+1) maxLen=i+1;
-            // }
+            if(sum==0){
+                maxLen=max(maxLen,i+1);
+            }
 
-            if(sums.find(sum)!=sums.end()){
-                maxLen=max(maxLen,i-sums[sum]);
+            else if(sums.find(sum)!=sums.end()){
+                if(i-sums[sum]>maxLen){
+                    maxLen=i-sums[sum];
+                }
             }
 
             else{
