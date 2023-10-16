aed543aaa690102d1bc37a1af9aba04772b56711
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-16 17:46:23 +0530

    Time: 0 ms (100%), Space: 6.8 MB (35.86%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> result;
-        // Initialize the first element of the row to 1.
         result.push_back(1);
-        // Calculate each element in the row using the binomial coefficient formula.
         for (int i = 1; i <= rowIndex; i++) {
-            long prevElement = result[i - 1];
-            // Use the formula C(r, i) = C(r, i-1) * (r - i + 1) / i
-            long currentElement = prevElement * (rowIndex - i + 1) / i;
-            result.push_back(static_cast<int>(currentElement));
+            long long res=result[i-1];
+            res*=(rowIndex-i+1);
+            res/=i;
+            result.push_back(res);
         }
-
         return result;
     }
 };
