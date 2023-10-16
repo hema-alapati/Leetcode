358c0631edb057e4cb51195b697127aba47bd43d
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-16 16:50:58 +0530

    Time: 0 ms (100%), Space: 6.9 MB (17.31%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,13 +1,24 @@
+// 60% Beast
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> res(1, 1);
-        long long prev = 1;
-        for(int k = 1; k <= rowIndex; k++) {
-            long long next_val = prev * (rowIndex - k + 1) / k;
-            res.push_back(next_val);
-            prev = next_val;
+        std::vector<std::vector<int>> triangle;
+
+        for (int i = 0; i <= rowIndex; i++) {
+            std::vector<int> row(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row[j] = 1;  // The first and last elements in each row are 1.
+                } else {
+                    int prevRow = i - 1;
+                    int leftVal = triangle[prevRow][j - 1];
+                    int rightVal = triangle[prevRow][j];
+                    row[j] = leftVal + rightVal;  // Sum of the two numbers above.
+                }
+            }
+            triangle.push_back(row);
         }
-        return res;
+
+        return triangle[rowIndex];
     }
 };
