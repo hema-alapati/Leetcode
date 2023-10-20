a79aa63d9786ef6f51dfafbdade7c98267ea687d
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 14:24:52 +0530

    Time: 0 ms (100%), Space: 9.4 MB (40.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,14 +5,10 @@ public:
         int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-                k--;
+                nums1[k--]=nums1[i--];
             }
             else{
-                nums1[k]=nums2[j];
-                j--;
-                k--;
+                nums1[k--]=nums2[j--];
             }
         }
         while(i>=0){
