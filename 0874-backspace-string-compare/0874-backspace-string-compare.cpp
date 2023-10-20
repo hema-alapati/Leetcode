35809e76bdef5805e516aab2a04faa22299a96dc
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 21:17:37 +0530

    Time: 3 ms (46.97%), Space: 6.5 MB (70.46%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         string s1, t1;
-        
         for (char c : s) {
             if (c == '#') {
-                if (!s1.empty()) {
-                    s1.pop_back();
-                }
-            } else {
-                s1.push_back(c);
-            }
+                if (!s1.empty()) s1.pop_back();
+            } else s1.push_back(c);
         }
-        
         for (char c : t) {
             if (c == '#') {
-                if (!t1.empty()) {
-                    t1.pop_back();
-                }
-            } else {
-                t1.push_back(c);
-            }
+                if (!t1.empty()) t1.pop_back();
+            } else t1.push_back(c);
         }
-        
         return s1 == t1;
     }
 };
