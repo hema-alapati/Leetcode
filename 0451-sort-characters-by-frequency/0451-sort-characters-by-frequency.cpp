2d92e65b64bfa1bf25db33f789d5f4a5f8e4ce11
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 17:57:07 +0530

    Time: 116 ms (5.05%), Space: 8.4 MB (88.89%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,18 +1,3 @@
-// struct node{
-//     char val;
-//     int freq;
-// };
-
-// class cmpr{
-//     public:
-//     bool operator()(node &a, node &b){
-//         if(a.freq<b.freq){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     string frequencySort(string s) {
@@ -20,26 +5,10 @@ public:
         for(int i = 0; i < s.size(); i++)
             mp[s[i]]++;
         
-         sort(s.begin(), s.end(), [&](char a, char b) -> bool {
+        sort(s.begin(), s.end(), [&](char a, char b) -> bool {
            return (mp[a] != mp[b]? mp[a] > mp[b] : a < b);
         });
         
         return s;
-        // priority_queue<node, vector<node>, cmpr> pq;
-        // for(auto &i:map){
-        //     node temp;
-        //     temp.val=i.first;
-        //     temp.freq=i.second;
-        //     int x = temp.freq;
-        //     while(x--){
-        //         pq.push(temp);
-        //     }
-        // }
-        // string ans;
-        // while(!pq.empty()){
-        //     ans+=pq.top().val;
-        //     pq.pop();
-        // }
-        // return ans;
     }
 };
