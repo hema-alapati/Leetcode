a6142381fc0b368d5f5e8216e93db726fb4486b1
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 14:38:14 +0530

    Time: 0 ms (100%), Space: 7.4 MB (57.97%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1 + str2 != str2 + str1) {
-            return "";
+        int len1 = str1.length();
+        int len2 = str2.length();
+        
+        if (len1 < len2) {
+            swap(str1, str2);
+            swap(len1, len2);
         }
-
-        int gcd = __gcd(str1.length(), str2.length());
-        return str1.substr(0, gcd);
+        
+        for (int i = len2; i >= 1; i--) {
+            if (len1 % i == 0 && len2 % i == 0) {
+                string commonDivisor = str1.substr(0, i);
+                string temp1 = commonDivisor, temp2 = commonDivisor;
+                
+                while (temp1.length() < len1) temp1 += commonDivisor;
+                while (temp2.length() < len2) temp2 += commonDivisor;
+                
+                if (temp1 == str1 && temp2 == str2) {
+                    return commonDivisor;
+                }
+            }
+        }
+        
+        return "";
     }
 };
