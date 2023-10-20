7a8fdbc5dcfd31a7f1dc424e905b5e08e64e9bc0
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 14:49:10 +0530

    Time: 0 ms (100%), Space: 7 MB (95.42%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
+    bool isCommonDivisor(string& str, string& candidate) {
+        int n = str.length();
+        int m = candidate.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (str[i] != candidate[i % m]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     string gcdOfStrings(string str1, string str2) {
         int len1 = str1.length();
         int len2 = str2.length();
-        
+
         if (len1 < len2) {
             swap(str1, str2);
             swap(len1, len2);
         }
-        
+
         for (int i = len2; i >= 1; i--) {
             if (len1 % i == 0 && len2 % i == 0) {
-                bool isCommonDivisor = true;
-                
-                for (int j = i; j < len1; j++) {
-                    if (str1[j] != str1[j % i]) {
-                        isCommonDivisor = false;
-                        break;
-                    }
-                }
-                
-                if (isCommonDivisor) {
-                    for (int j = 0; j < len2; j++) {
-                        if (str2[j] != str1[j % i]) {
-                            isCommonDivisor = false;
-                            break;
-                        }
-                    }
-                }
-                
-                if (isCommonDivisor) {
-                    return str1.substr(0, i);
+                string commonDivisor = str1.substr(0, i);
+
+                if (isCommonDivisor(str1, commonDivisor) && isCommonDivisor(str2, commonDivisor)) {
+                    return commonDivisor;
                 }
             }
         }
-        
+
         return "";
     }
 };
