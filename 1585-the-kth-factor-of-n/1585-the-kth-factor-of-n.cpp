5dbf685b7ee4b99a49e07243830f19131dafa080
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-20 15:45:44 +0530

    Time: 0 ms (100%), Space: 6.4 MB (29.87%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     int kthFactor(int n, int k) {
         vector<int>res;
+        int kth=0;
         for(int i = 1; i <= n; ++i) {
             if(n % i == 0){
-                res.push_back(i);
+                kth++;
+                if(kth==k){
+                    return i;
+                }
+                // res.push_back(i);
             }
         }
-        sort(res.begin(),res.end());
-        if(k-1<res.size()){
-            return res[k-1];
-        }
-        else return -1;
+        // sort(res.begin(),res.end());
+        // if(k-1<res.size()){
+        //     return res[k-1];
+        // }
+        return -1;
     }
 };
