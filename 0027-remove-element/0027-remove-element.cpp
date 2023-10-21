4af678b7cdb3949eed37132acb02f95674b93671
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-21 20:41:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (43.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int k=0;
-        for(auto it:nums){
-            if(it!=val){
-                nums[k++]=it;
-            }
-        }
+        for(auto it:nums){if(it!=val) nums[k++]=it;}
         return k;
     }
 };
