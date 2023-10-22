681d6be580f0e86808d41d309f0d0af60f08302c
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-22 12:19:26 +0530

    Time: 27 ms (85.73%), Space: 26.3 MB (17.11%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         if (timeSeries.empty()) return 0;
+        
+        int totalDuration = 0;
+        int n = timeSeries.size();
 
-        int totalDuration = duration;
-        int endTime = timeSeries[0] + duration;
-
-        for (int i = 1; i < timeSeries.size(); i++) {
-            totalDuration += min(duration, timeSeries[i] - timeSeries[i - 1]);
-            endTime = timeSeries[i] + duration;
+        for (int i = 1; i < n; i++) {
+            totalDuration += min(timeSeries[i] - timeSeries[i - 1], duration);
         }
 
+        totalDuration += duration;
         return totalDuration;
     }
 };
