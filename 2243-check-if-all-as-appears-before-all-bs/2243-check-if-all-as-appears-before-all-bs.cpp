b5c2f73674655d6840113f80431f312e1a59e36f
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-11-09 22:33:51 +0530

    Time: 3 ms (44%), Space: 6.6 MB (45.24%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool checkString(string s) {
-        int n=s.size();
         int k=0;
         while(s[k]=='a') k++;
-        for(char i=k;i<n-1;i++){
+        for(char i=k;i<s.size()-1;i++){
             if(s[i]=='b'){
                 if(s[i+1]=='b') continue;
                 else return false;
