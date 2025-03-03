01dc50eefe0cc022212e347c5ea47979d096c4e1
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2025-03-04 00:43:19 +0530

    Time: 4 ms (100%), Space: 68.2 MB (35.78%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,37 +1,30 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int count = 0;
-        List<Integer> ans = new ArrayList<>();
+        int count=0;
+        int index=0;
+        int[] ans=new int[nums.length];
 
-        // First loop: Count occurrences of pivot and add elements < pivot
-        for (int num : nums) {
-            if (num == pivot) count++;
-            else if (num < pivot) ans.add(num);
-        }
-
-        // Second loop: Add elements > pivot, inserting pivot elements in between
-        for (int num : nums) {
-            if (num > pivot) {
-                while (count > 0) {
-                    ans.add(pivot);
-                    count--;
-                }
-                ans.add(num);
+        for(int num:nums){
+            if(num==pivot) count++;
+            else if(num<pivot) {
+                ans[index]=num;
+                index++;
             }
         }
 
-        // If any pivot elements remain, add them at the end
-        while (count > 0) {
-            ans.add(pivot);
+        while(count>0){
+            ans[index]=pivot;
+            index++;
             count--;
         }
 
-        // Convert List<Integer> back to int[]
-        int[] result = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
+        for(int num:nums){
+            if(num>pivot){
+                ans[index]=num;
+                index++;
+            }
         }
 
-        return result;
+        return ans;
     }
 }
