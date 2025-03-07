780f59ffd1321c05333f5827f81478fac5d20c62
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2025-03-07 16:13:31 +0530

    Time: 199 ms (54.94%), Space: 12.8 MB (38.54%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;
-
-        vector<bool> primeArray(n, true);
-        primeArray[0] = primeArray[1] = false;
-
-        for (int i = 2; i * i < n; i++) {
+        int count=0;
+        vector<bool> primeArray(n+1,1);
+        if(n<=2) return 0;
+         for (int i = 2; i * i <= n; i++) {
             if (primeArray[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    primeArray[j] = false;
+                    primeArray[j] = 0; 
                 }
             }
         }
-
-        return count(primeArray.begin(), primeArray.end(), true);
+        for(int i=2;i<n;i++){
+            if(primeArray[i]==1) count++;
+        }
+        return count;
     }
 };
