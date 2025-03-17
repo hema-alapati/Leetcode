931c0ddfc24d7b0f86cf6d3de7d1841ba9d00242
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2025-03-18 00:18:50 +0530

    Time: 4 ms (74.89%), Space: 44.3 MB (71.91%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,19 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean divideArray(int[] nums) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        // Count occurrences of each number
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i += 2) {
+            if (nums[i] != nums[i + 1]) return false;
         }
-
-        // Check if all numbers appear an even number of times
-        for (int count : freq.values()) {
-            if (count % 2 != 0) {
-                return false;
-            }
-        }
-
         return true;
     }
 }
