77b3a801c01a2663eb0554720b9d3d13c3b2ce13
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2025-05-14 17:01:14 +0530

    Time: 143 ms (51.22%), Space: 64.1 MB (14.63%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
@@ -1,25 +1,31 @@
 class Solution {
     static final int MOD = 1_000_000_007;
-    int[][] dp = new int[26][100_001]; // dp[char][t]
 
     public int lengthAfterTransformations(String s, int t) {
+        int[][] dp = new int[26][t + 1];
+
+        // Base case: dp[c][0] = 1 for all characters
+        for (int c = 0; c < 26; c++) {
+            dp[c][0] = 1;
+        }
+
+        // Fill the dp table
+        for (int i = 1; i <= t; i++) {
+            for (int c = 25; c >= 0; c--) {
+                if (c == 25) { // 'z'
+                    dp[c][i] = (dp[0][i - 1] + dp[1][i - 1]) % MOD;
+                } else {
+                    dp[c][i] = dp[c + 1][i - 1];
+                }
+            }
+        }
+
+        // Final result: sum dp[c][t] for each character c in s
         int result = 0;
-        for (char c : s.toCharArray()) {
-            result = (result + dfs(c - 'a', t)) % MOD;
+        for (char ch : s.toCharArray()) {
+            result = (result + dp[ch - 'a'][t]) % MOD;
         }
+
         return result;
-    }
-
-    private int dfs(int c, int t) {
-        if (t == 0) return 1;
-        if (dp[c][t] != 0) return dp[c][t];
-
-        if (c == 25) { // 'z'
-            dp[c][t] = (dfs(0, t - 1) + dfs(1, t - 1)) % MOD;
-        } else {
-            dp[c][t] = dfs(c + 1, t - 1);
-        }
-
-        return dp[c][t];
     }
 }
